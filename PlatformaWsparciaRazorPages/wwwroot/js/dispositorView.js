a178b22b413bb597304df2dd20f2d7f6dbdeadbc
7Bubun <j.grenda39@gmail.com>
2021-12-02 23:36:29 +0100

    Implement most of Dispositor's view

--- PlatformaWsparciaRazorPages/wwwroot/js/dispositorView.js
+++ PlatformaWsparciaRazorPages/wwwroot/js/dispositorView.js
@@ -1,19 +1,130 @@
 ﻿//mock
 let listOfPeopleInNeed = [
-    { 'name': 'Janusz', 'surname': 'Januszewski', 'streetname': 'Szkolna', 'town': 'Wrocław', 'buildingID': '44', 'flatID': '', 'zipCode': '00-234' },
-    { 'name': 'Mirek', 'surname': 'Mirkowski', 'streetname': 'Słoneczna', 'town': 'Białystok', 'buildingID': '1', 'flatID': '55', 'zipCode': '04-234' },
-    { 'name': 'Zenon', 'surname': 'Zenoniuk', 'streetname': 'Einstrasse', 'town': 'Berlin', 'buildingID': '12', 'flatID': '56', 'zipCode': '00-234' },
+    {
+        'id': 1,
+        'firstName': 'Janusz',
+        'lastName': 'Januszewski',
+        'address': 'Szkolna',
+        'town': 'Wrocław',
+        'zipCode': '00-234',
+        'phoneNumber': '2134234',
+        'email': 'adwa@dwa',
+        'age': 44,
+        'townPopulation': 123,
+        'householdSize': 40,
+        'financialSituation': 3,
+        'healthSituation': 3,
+        'standardOfLiving': 3,
+        'familySituation': 3,
+        'chronicIlnesses': false,
+        'dependence': true,
+        'products': [
+            ['catA', 'product1'],
+            ['catA', 'product2'],
+            ['catB', 'product3']
+        ],
+        'priority': 3
+    },
+    {
+        'id': 9,
+        'firstName': 'Zenon',
+        'lastName': 'Zenoniuk',
+        'address': 'Słoneczna 44',
+        'town': 'Białystok',
+        'zipCode': '00-23123',
+        'phoneNumber': '000000000',
+        'email': 'adwa@dwawdawdwad',
+        'age': 32,
+        'townPopulation': 1233,
+        'householdSize': 40,
+        'financialSituation': 4,
+        'healthSituation': 5,
+        'standardOfLiving': 4,
+        'familySituation': 3,
+        'chronicIlnesses': true,
+        'dependence': true,
+        'products': [
+            ['catC', 'productD'],
+            ['catA', 'productE'],
+            ['catD', 'productF']
+        ],
+        'priority': 2
+    },
+    {
+        'id': 3,
+        'firstName': 'Mirek',
+        'lastName': 'Januszewski',
+        'address': 'Einstrasse',
+        'town': 'Berlin',
+        'zipCode': '05-551',
+        'phoneNumber': '277234',
+        'email': 'miro@dwa.hehe',
+        'age': 44,
+        'townPopulation': 123,
+        'householdSize': 40,
+        'financialSituation': 1,
+        'healthSituation': 2,
+        'standardOfLiving': 1,
+        'familySituation': 2,
+        'chronicIlnesses': false,
+        'dependence': false,
+        'products': [['catA', 'product1']],
+        'priority': 5
+    }
 ];
 
-let formatAddressOfPersonInNeed = (person) => {
-    let result = person.streetname + ' ' + person.buildingID;
+//mock
+let listOfHelpers = [
+    {
+        'id': 1,
+        'firstName': 'Jan',
+        'lastName': 'Pomocnik',
+        'phoneNumber': '234234',
+        'email': 'hehe@haha.com',
+        'town': 'Berlin',
+        'zipCode': '08-200',
+        'products': [
+            ['catC', 'productD'],
+            ['catA', 'productE'],
+            ['catD', 'productF']
+        ]
+    },
+    {
+        'id': 5,
+        'firstName': 'Andrzej',
+        'lastName': 'Nazwiskowski',
+        'phoneNumber': '2348674',
+        'email': 'andy@haha.com',
+        'town': 'Warsaw',
+        'zipCode': '00-200',
+        'products': [
+            ['catC', 'productD'],
+            ['catZ', 'product56']
+        ]
+    },
+    {
+        'id': 7,
+        'firstName': 'Jędrzej',
+        'lastName': 'Pomocny',
+        'phoneNumber': '234242434',
+        'email': 'jendrek@o2.pl',
+        'town': 'Poznań',
+        'zipCode': '90-907',
+        'products': [
+            ['catT', 'productZ'],
+            ['catD', 'productY'],
+            ['catD', 'productX']
+        ]
+    },
+];
 
-    if (person.flatID !== '') {
-        result += ' ' + person.flatID;
-    }
+let chosenPersonInNeed, chosenHelper;
 
-    result += ' ' + person.zipCode + ' ' + person.town;
-    return result;
+let createTableCell = (className, innerHTML) => {
+    let tableCell = document.createElement('td');
+    tableCell.className = className;
+    tableCell.innerHTML = innerHTML;
+    return tableCell;
 }
 
 let loadPeopleInNeed = (peopleInNeed) => {
@@ -25,33 +136,108 @@ let loadPeopleInNeed = (peopleInNeed) => {
         let row = document.createElement('tr');
         row.className = 'data-row';
 
-        let nameCell = document.createElement('td');
-        nameCell.className = 'namecell';
-        nameCell.innerHTML = personInNeed.name;
-        row.appendChild(nameCell);
+        let idCell = createTableCell(
+            'id-cell',
+            personInNeed.id
+        );
 
-        //to refactor (repeated code)
-        let surnameCell = document.createElement('td');
-        surnameCell.className = 'namecell';
-        surnameCell.innerHTML = personInNeed.surname;
-        row.appendChild(surnameCell);
+        let locationCell = createTableCell(
+            'location-cell',
+            personInNeed.zipCode + ' ' + personInNeed.town
+        );
 
-        let addressCell = document.createElement('td');
-        addressCell.className = 'addresscell';
-        addressCell.innerHTML = formatAddressOfPersonInNeed(personInNeed);
-        row.appendChild(addressCell);
+        let priorityCell = createTableCell(
+            'priority-cell',
+            personInNeed.priority
+        );
 
-        let button = document.createElement('button');
-        button.innerHTML = 'Add';
-        button.onclick = () => alert(personInNeed.surname);
+        let buttonsCell = createTableCell(
+            'button-cell',
+            ''
+        );
 
-        let buttonCell = document.createElement('td');
-        buttonCell.className = 'buttoncell';
-        buttonCell.appendChild(button);
-        row.appendChild(buttonCell);
+        let addButton = document.createElement('button');
+        addButton.innerHTML = 'Add';
+        addButton.onclick = () => {
+            chosenPersonInNeed = personInNeed;
+
+            document.getElementsByClassName('matcher-person-in-need')[1]
+                .innerHTML = personInNeed.firstName + ' ' + personInNeed.lastName + ' ' + personInNeed.id;
+        };
+
+        buttonsCell.appendChild(addButton);
+
+        let showButton = document.createElement('button');
+        showButton.innerHTML = 'Show';
+        showButton.onclick = () => alert(personInNeed.products);
+        buttonsCell.appendChild(showButton);
+
+        row.appendChild(idCell);
+        row.appendChild(locationCell);
+        row.appendChild(priorityCell);
+        row.appendChild(buttonsCell);
 
         observableList.appendChild(row);
     }
+};
+
+let loadHelpers = (helpers) => {
+    let helpersTable = document.getElementById('helpers');
+
+    for (let i = 0; i < helpers.length; i++) {
+        let helper = helpers[i];
+
+        let row = document.createElement('tr');
+        row.className = 'data-row';
+
+        let idCell = createTableCell(
+            'helper-id-cell',
+            helper.id
+        );
+
+        let locationCell = createTableCell(
+            'helper-location-cell',
+            helper.zipCode + ' ' + helper.town
+        );
+
+        let productsCell = createTableCell(
+            'helper-products-cell',
+            helper.products[0]
+        );
+
+        let actionCell = createTableCell(
+            'helper-button-cell',
+            ''
+        );
+
+        let addButton = document.createElement('button');
+        addButton.innerHTML = 'Add';
+        addButton.onclick = () => {
+            chosenHelper = helper;
+
+            document.getElementsByClassName('matcher-helper')[1]
+                .innerHTML = helper.firstName + ' ' + helper.lastName + ' ' + helper.id;
+        };
+
+        let showButton = document.createElement('button');
+        showButton.innerHTML = 'Show';
+        showButton.onclick = () => alert(helper);
+
+        actionCell.appendChild(addButton);
+        actionCell.appendChild(showButton);
+
+        row.appendChild(idCell);
+        row.appendChild(locationCell);
+        row.appendChild(productsCell);
+        row.appendChild(actionCell);
+
+        helpersTable.appendChild(row);
+    }
+};
+
+//TODO
+let match = () => {
+    alert(chosenPersonInNeed.lastName + ' ' + chosenHelper.lastName);
 };
 
 let main = () => {
@@ -61,6 +247,11 @@ let main = () => {
         listOfPeopleInNeed.push(listOfPeopleInNeed[i % 3]);
     }
 
+    //mock
+    for (let i = 0; i < 20; i++) {
+        listOfHelpers.push(listOfHelpers[i % 3]);
+    }
 
     loadPeopleInNeed(listOfPeopleInNeed);
+    loadHelpers(listOfHelpers);
 };
