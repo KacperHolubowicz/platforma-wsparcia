ca78837d1e568e884f92b11a2f207726c39801d8
7Bubun <j.grenda39@gmail.com>
2021-12-03 14:53:04 +0100

    Finish implementing logic

--- PlatformaWsparciaRazorPages/wwwroot/js/dispositorView.js
+++ PlatformaWsparciaRazorPages/wwwroot/js/dispositorView.js
@@ -1,121 +1,190 @@
 ﻿//mock
 let listOfPeopleInNeed = [
     {
-        'id': 1,
+        'personInNeedID': 1,
         'firstName': 'Janusz',
         'lastName': 'Januszewski',
-        'address': 'Szkolna',
-        'town': 'Wrocław',
-        'zipCode': '00-234',
-        'phoneNumber': '2134234',
-        'email': 'adwa@dwa',
-        'age': 44,
-        'townPopulation': 123,
-        'householdSize': 40,
-        'financialSituation': 3,
-        'healthSituation': 3,
-        'standardOfLiving': 3,
-        'familySituation': 3,
-        'chronicIlnesses': false,
-        'dependence': true,
+        'contactDetails': {
+            'phoneNumber': '2134234',
+            'email': 'adwa@dwa',
+        },
+        'personalDetails': {
+            'town': 'Wrocław',
+            'address': 'Szkolna',
+            'postcode': '00-234'
+        },
         'products': [
-            ['catA', 'product1'],
-            ['catA', 'product2'],
-            ['catB', 'product3']
+            {
+                'productType': 'food',
+                'productName': 'boorgir'
+            },
+            {
+                'productType': 'other',
+                'productName': 'toothpaste'
+            }
         ],
-        'priority': 3
+        'lifeSituation': {
+            'description': 'ribfuabduwavbfueawyvfyweavfywaevf',
+            'priority': 3
+        },
+        'lifeSituationClassification': {
+            'age': 44,
+            'townPopulation': 123,
+            'householdSize': 40,
+            'financialSituation': 3,
+            'healthSituation': 3,
+            'standardOfLiving': 3,
+            'familySituation': 3,
+            'chronicIlnesses': false,
+            'dependence': true
+        }
     },
     {
-        'id': 9,
+        'personInNeedID': 5,
         'firstName': 'Zenon',
         'lastName': 'Zenoniuk',
-        'address': 'Słoneczna 44',
-        'town': 'Białystok',
-        'zipCode': '00-23123',
-        'phoneNumber': '000000000',
-        'email': 'adwa@dwawdawdwad',
-        'age': 32,
-        'townPopulation': 1233,
-        'householdSize': 40,
-        'financialSituation': 4,
-        'healthSituation': 5,
-        'standardOfLiving': 4,
-        'familySituation': 3,
-        'chronicIlnesses': true,
-        'dependence': true,
+        'contactDetails': {
+            'phoneNumber': '0123798',
+            'email': 'zenus@ayaya',
+        },
+        'personalDetails': {
+            'town': 'Warsaw',
+            'address': 'Emilii Plater 9999',
+            'postcode': '00-204'
+        },
         'products': [
-            ['catC', 'productD'],
-            ['catA', 'productE'],
-            ['catD', 'productF']
+            {
+                'productType': 'food',
+                'productName': 'pizzah'
+            },
+            {
+                'productType': 'food',
+                'productName': 'tea'
+            }
         ],
-        'priority': 2
+        'lifeSituation': {
+            'description': 'ribfuabiiiiiiiiiiiiiiiiiiiwaevf',
+            'priority': 1
+        },
+        'lifeSituationClassification': {
+            'age': 29,
+            'townPopulation': 2000000,
+            'householdSize': 40,
+            'financialSituation': 4,
+            'healthSituation': 4,
+            'standardOfLiving': 3,
+            'familySituation': 2,
+            'chronicIlnesses': false,
+            'dependence': false
+        }
     },
     {
-        'id': 3,
+        'personInNeedID': 11,
         'firstName': 'Mirek',
-        'lastName': 'Januszewski',
-        'address': 'Einstrasse',
-        'town': 'Berlin',
-        'zipCode': '05-551',
-        'phoneNumber': '277234',
-        'email': 'miro@dwa.hehe',
-        'age': 44,
-        'townPopulation': 123,
-        'householdSize': 40,
-        'financialSituation': 1,
-        'healthSituation': 2,
-        'standardOfLiving': 1,
-        'familySituation': 2,
-        'chronicIlnesses': false,
-        'dependence': false,
-        'products': [['catA', 'product1']],
-        'priority': 5
+        'lastName': 'Mirkowski',
+        'contactDetails': {
+            'phoneNumber': '78634765',
+            'email': 'miro@ehehe.z',
+        },
+        'personalDetails': {
+            'town': 'Berlin',
+            'address': 'Einstrasse 42',
+            'postcode': '07-001'
+        },
+        'products': [
+            {
+                'productType': 'other',
+                'productName': 'tissues'
+            }
+        ],
+        'lifeSituation': {
+            'description': 'ribfuabduwavbfueawyvfyweavfywaevf',
+            'priority': 3
+        },
+        'lifeSituationClassification': {
+            'age': 48,
+            'townPopulation': 9876543,
+            'householdSize': 50,
+            'financialSituation': 4,
+            'healthSituation': 2,
+            'standardOfLiving': 4,
+            'familySituation': 5,
+            'chronicIlnesses': true,
+            'dependence': false
+        }
     }
 ];
 
 //mock
 let listOfHelpers = [
     {
-        'id': 1,
-        'firstName': 'Jan',
-        'lastName': 'Pomocnik',
-        'phoneNumber': '234234',
-        'email': 'hehe@haha.com',
-        'town': 'Berlin',
-        'zipCode': '08-200',
+        'donorID': 15,
+        'firstName': 'Adam',
+        'lastName': 'Pomagier',
+        'contactDetails': {
+            'phoneNumber': '241234234',
+            'email': 'xd@quantum.q'
+        },
+        'personalDetails': {
+            'town': 'Vladivoshtock',
+            'address': 'Vladimirova 67',
+            'postcode': '30-921'
+        },
         'products': [
-            ['catC', 'productD'],
-            ['catA', 'productE'],
-            ['catD', 'productF']
+            {
+                'productType': 'food',
+                'productName': 'meat'
+            }
         ]
     },
     {
-        'id': 5,
-        'firstName': 'Andrzej',
-        'lastName': 'Nazwiskowski',
-        'phoneNumber': '2348674',
-        'email': 'andy@haha.com',
-        'town': 'Warsaw',
-        'zipCode': '00-200',
+        'donorID': 23,
+        'firstName': 'Joanna',
+        'lastName': 'Asiowska',
+        'contactDetails': {
+            'phoneNumber': '44444555',
+            'email': 'coo@haha.com'
+        },
+        'personalDetails': {
+            'town': 'Talin',
+            'address': 'street 234',
+            'postcode': '11-279'
+        },
         'products': [
-            ['catC', 'productD'],
-            ['catZ', 'product56']
+            {
+                'productType': 'medicine',
+                'productName': 'ABC'
+            },
+            {
+                'productType': 'food',
+                'productName': 'spaghetti code'
+            }
         ]
     },
     {
-        'id': 7,
-        'firstName': 'Jędrzej',
-        'lastName': 'Pomocny',
-        'phoneNumber': '234242434',
-        'email': 'jendrek@o2.pl',
-        'town': 'Poznań',
-        'zipCode': '90-907',
+        'donorID': 44,
+        'firstName': 'Adam',
+        'lastName': 'Mickiewicz',
+        'contactDetails': {
+            'phoneNumber': '4014',
+            'email': 'konrad@dziady.lt'
+        },
+        'personalDetails': {
+            'town': 'Wilno',
+            'address': 'Soplicowska 44',
+            'postcode': '44-404'
+        },
         'products': [
-            ['catT', 'productZ'],
-            ['catD', 'productY'],
-            ['catD', 'productX']
+            {
+                'productType': 'books',
+                'productName': 'Dziady cz. 3'
+            },
+            {
+                'productType': 'books',
+                'productName': 'Pan Tadeusz'
+            }
         ]
-    },
+    }
 ];
 
 let chosenPersonInNeed, chosenHelper;
@@ -127,6 +196,58 @@ let createTableCell = (className, innerHTML) => {
     return tableCell;
 }
 
+let insertListItem = (elementID, tag, content) => {
+    document.getElementById(elementID).innerHTML = tag + ': ' + content;
+};
+
+let addPersonInNeedToMatch = (personInNeed) => () => {
+    chosenPersonInNeed = personInNeed;
+
+    document.getElementsByClassName('matcher-person-in-need')[1]
+        .innerHTML = personInNeed.firstName + ' ' + personInNeed.lastName + ' ' + personInNeed.personInNeedID;
+};
+
+//arrow func that returns arrow func
+let showPersonInNeed = (personInNeed) => () => {
+    let peopleInNeedModalWindow = document.getElementsByClassName('modal-window')[0];
+    peopleInNeedModalWindow.style.display = 'block';
+
+    insertListItem('in-need-id', 'ID', personInNeed.personInNeedID);
+    insertListItem('in-need-first-name', 'First name', personInNeed.firstName);
+    insertListItem('in-need-last-name', 'Last name', personInNeed.lastName);
+    insertListItem('in-need-address', 'Address', personInNeed.personalDetails.address);
+    insertListItem('in-need-town', 'Town', personInNeed.personalDetails.town);
+    insertListItem('in-need-zip-code', 'Zip code', personInNeed.personalDetails.postcode);
+    insertListItem('in-need-phone-number', 'Phone number', personInNeed.contactDetails.phoneNumber);
+    insertListItem('in-need-email', 'E-mail', personInNeed.contactDetails.email);
+    insertListItem('in-need-age', 'Age', personInNeed.lifeSituationClassification.age);
+
+    let lsc = personInNeed.lifeSituationClassification;
+    insertListItem('in-need-town-population', 'Town population', lsc.townPopulation);
+    insertListItem('in-need-household-size', 'Size of household', lsc.householdSize);
+    insertListItem('in-need-financial-situation', 'Financial situation (1-5)', lsc.financialSituation);
+    insertListItem('in-need-health-situation', 'Health situation (1-5)', lsc.healthSituation);
+    insertListItem('in-need-standard-of-living', 'Standard of living (1-5)', lsc.standardOfLiving);
+    insertListItem('in-need-family-situation', 'Family situation (1-5)', lsc.familySituation);
+    insertListItem('in-need-chronic-illnesses', 'Chronic illnesses', lsc.chronicIlnesses);
+    insertListItem('in-need-dependence', 'Dependence', lsc.dependence);
+    insertListItem('in-need-priority', 'Suggested priority', personInNeed.lifeSituation.priority);
+
+    //DESCRIPTION! EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+    insertListItem('in-need-description', 'Description', personInNeed.lifeSituation.description);
+
+    let inNeedProductList = document.getElementById('in-need-product-list');
+    inNeedProductList.innerHTML = '';
+
+    for (let i = 0; i < personInNeed.products.length; i++) {
+        let item = document.createElement('li');
+        item.innerHTML = personInNeed.products[i].productName + ' (' + personInNeed.products[i].productType + ')';
+        inNeedProductList.appendChild(item);
+    }
+
+    document.getElementById('add-person-in-need-from-modal').onclick = addPersonInNeedToMatch(personInNeed);
+};
+
 let loadPeopleInNeed = (peopleInNeed) => {
     let observableList = document.getElementById('people-in-need');
 
@@ -136,40 +257,25 @@ let loadPeopleInNeed = (peopleInNeed) => {
         let row = document.createElement('tr');
         row.className = 'data-row';
 
-        let idCell = createTableCell(
-            'id-cell',
-            personInNeed.id
-        );
+        let idCell = createTableCell('id-cell', personInNeed.personInNeedID);
 
         let locationCell = createTableCell(
             'location-cell',
-            personInNeed.zipCode + ' ' + personInNeed.town
+            personInNeed.personalDetails.postcode + ' ' + personInNeed.personalDetails.town
         );
 
-        let priorityCell = createTableCell(
-            'priority-cell',
-            personInNeed.priority
-        );
-
-        let buttonsCell = createTableCell(
-            'button-cell',
-            ''
-        );
+        let priorityCell = createTableCell('priority-cell', personInNeed.lifeSituation.priority);
+        let buttonsCell = createTableCell('button-cell', '');
 
         let addButton = document.createElement('button');
         addButton.innerHTML = 'Add';
-        addButton.onclick = () => {
-            chosenPersonInNeed = personInNeed;
-
-            document.getElementsByClassName('matcher-person-in-need')[1]
-                .innerHTML = personInNeed.firstName + ' ' + personInNeed.lastName + ' ' + personInNeed.id;
-        };
-
-        buttonsCell.appendChild(addButton);
+        addButton.onclick = addPersonInNeedToMatch(personInNeed);
 
         let showButton = document.createElement('button');
         showButton.innerHTML = 'Show';
-        showButton.onclick = () => alert(personInNeed.products);
+        showButton.onclick = showPersonInNeed(personInNeed);
+
+        buttonsCell.appendChild(addButton);
         buttonsCell.appendChild(showButton);
 
         row.appendChild(idCell);
@@ -181,6 +287,36 @@ let loadPeopleInNeed = (peopleInNeed) => {
     }
 };
 
+let addHelperToMatch = (helper) => () => {
+    chosenHelper = helper;
+    document.getElementsByClassName('matcher-helper')[1]
+        .innerHTML = helper.firstName + ' ' + helper.lastName + ' ' + helper.donorID;
+};
+
+//arrow func that returns arrow func
+let showHelper = (helper) => () => {
+    document.getElementsByClassName('right-modal-window')[0].style.display = 'block';
+
+    insertListItem('helper-id', 'ID', helper.donorID);
+    insertListItem('helper-first-name', 'First name', helper.firstName);
+    insertListItem('helper-last-name', 'Last name', helper.lastName);
+    insertListItem('helper-phone-number', 'Phone number', helper.contactDetails.phoneNumber);
+    insertListItem('helper-email', 'E-mail', helper.contactDetails.email);
+    insertListItem('helper-town', 'Town', helper.personalDetails.town);
+    insertListItem('helper-zip-code', 'Zip code', helper.personalDetails.postcode);
+
+    let helperProductList = document.getElementById('helper-product-list');
+    helperProductList.innerHTML = '';
+
+    for (let i = 0; i < helper.products.length; i++) {
+        let item = document.createElement('li');
+        item.innerHTML = helper.products[i].productName + ' (' + helper.products[i].productType + ')';
+        helperProductList.appendChild(item);
+    }
+
+    document.getElementById('add-helper-from-modal').onclick = addHelperToMatch(helper);
+};
+
 let loadHelpers = (helpers) => {
     let helpersTable = document.getElementById('helpers');
 
@@ -190,38 +326,23 @@ let loadHelpers = (helpers) => {
         let row = document.createElement('tr');
         row.className = 'data-row';
 
-        let idCell = createTableCell(
-            'helper-id-cell',
-            helper.id
-        );
+        let idCell = createTableCell('helper-id-cell', helper.donorID);
 
         let locationCell = createTableCell(
             'helper-location-cell',
-            helper.zipCode + ' ' + helper.town
+            helper.personalDetails.postcode + ' ' + helper.personalDetails.town
         );
 
-        let productsCell = createTableCell(
-            'helper-products-cell',
-            helper.products[0]
-        );
-
-        let actionCell = createTableCell(
-            'helper-button-cell',
-            ''
-        );
+        let productsCell = createTableCell('helper-products-cell', helper.products[0].productType);
+        let actionCell = createTableCell('helper-button-cell', '');
 
         let addButton = document.createElement('button');
         addButton.innerHTML = 'Add';
-        addButton.onclick = () => {
-            chosenHelper = helper;
-
-            document.getElementsByClassName('matcher-helper')[1]
-                .innerHTML = helper.firstName + ' ' + helper.lastName + ' ' + helper.id;
-        };
+        addButton.onclick = addHelperToMatch(helper);
 
         let showButton = document.createElement('button');
         showButton.innerHTML = 'Show';
-        showButton.onclick = () => alert(helper);
+        showButton.onclick = showHelper(helper);
 
         actionCell.appendChild(addButton);
         actionCell.appendChild(showButton);
@@ -239,6 +360,8 @@ let loadHelpers = (helpers) => {
 let match = () => {
     alert(chosenPersonInNeed.lastName + ' ' + chosenHelper.lastName);
 };
+
+let closeModalWindow = (className) => document.getElementsByClassName(className)[0].style.display = 'none';
 
 let main = () => {
 
